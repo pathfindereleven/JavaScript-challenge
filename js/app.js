883af0efc48f6b2b1ed0ec99aeb45c5e5954edb5
pathfindereleven <njmizerny@rocketmail.com>
2023-09-20 22:54:16 -0700

    a wee bit smaller

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const url = d3.select("q").text()
 function s() {   
     d3.json(url).then(function(d){
         (d.names).forEach((i) => {(d3.select('#se')).append("option").text(i).property("value", i)}); b(d.names[0])})} ;        
