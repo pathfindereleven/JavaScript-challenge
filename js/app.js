bd0c61dac1957a3aeb565e5fea08ba6632f132fa
pathfindereleven <njmizerny@rocketmail.com>
2023-09-20 21:51:42 -0700

    dense

--- js/app.js
+++ js/app.js
@@ -1,22 +1,15 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-function startup() {   
+function s() {   
     d3.json(url).then(function(data){
-        (data.names).forEach((name) => {(d3.select('#selDataset')).append("option").text(name).property("value", name)}); // populate drop down menu
-        build_func(data.names[0])})} ;  // use first value for defualt display
-function build_func(x) {           // build function
-    d3.json(url).then((data) => { // pull data
-        let match = (data.metadata).filter(result => result.id == x);
-        d3.select("#sample-metadata").html("");         // clear demo panel
-        Object.entries(match[0]).forEach(([key,value]) => { d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`)});  // append data by key value pair to demogrphics, end demographics portion start bar portion
-        let match1 = (data.samples).filter(result => result.id == x);   // match data to drop down selected id  
-        let ybar = match1[0].otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();   // select top 10
-        let xbar = match1[0].sample_values.slice(0,10).reverse();                    // select top 10
-        let labels_bar = match1[0].otu_labels.slice(0,10).reverse();                  // select top 10
-        let trace1 = {x: xbar, y: ybar, text: labels_bar, type: "bar", orientation: "h" };  // build trace for bar chart
-        Plotly.newPlot("bar", [trace1])  // end bar portion start bubble
-        let trace2= {x:match1[0].otu_ids, y: match1[0].sample_values, text: match1[0].otu_labels, mode: "markers",marker: {size: match1[0].sample_values, color: match1[0].otu_ids, colorscale: "Earth"} }; // buble trace
-        let layout = {title: "Bacteria Count", hovermode: "closest",xaxis: {title: "OTU ID"}};
-        Plotly.newPlot("bubble", [trace2], layout)})};   // plot bubble chart
-startup();  // change option function changed to build_func in html
+        (data.names).forEach((name) => {(d3.select('#se')).append("option").text(name).property("value", name)}); b(data.names[0])})} ;        
+function b(x) {           
+    d3.json(url).then((data) => { 
+        let n = (data.metadata).filter(result => result.id == x); d3.select("#sa").html("");         
+        Object.entries(n[0]).forEach(([key,value]) => { d3.select("#sa").append("h5").text(`${key}: ${value}`)}); 
+        let m = (data.samples).filter(result => result.id == x); let y = m[0].otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();     
+        let z = m[0].sample_values.slice(0,10).reverse(); let p = m[0].otu_labels.slice(0,10).reverse(); let t = {x: z, y: y, text: p, type: "bar", orientation: "h" };                                       
+        let u= {x:m[0].otu_ids, y: m[0].sample_values, text: m[0].otu_labels, mode: "markers",marker: {size: m[0].sample_values, color: m[0].otu_ids, colorscale: "Earth"} }; 
+        let l = {title: "Bacteria Count", hovermode: "closest",xaxis: {title: "OTU ID"}}; Plotly.newPlot("bubble", [u], l); Plotly.newPlot("bar", [t])})}; s();   
+  
 
 
